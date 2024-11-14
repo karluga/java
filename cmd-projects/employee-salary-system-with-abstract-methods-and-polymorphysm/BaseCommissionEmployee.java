bb3efb58f56d33754c2eb9b495c4383854fa992f
karluga <kaarlisbrakis@gmail.com>
2024-11-14 23:44:11 +0200

    added fixed bonus rate

--- cmd-projects/employee-salary-system-with-abstract-methods-and-polymorphysm/BaseCommissionEmployee.java
+++ cmd-projects/employee-salary-system-with-abstract-methods-and-polymorphysm/BaseCommissionEmployee.java
@@ -1,22 +1,18 @@
-
-
 class BaseCommissionEmployee extends Employee {
     private double baseSalary;
     private double totalSales;
     private double commissionRate;
-    private double bonus;
+    private static final double BONUS_RATE = 0.10; // Fixed 10 percent
 
-    public BaseCommissionEmployee(String firstName, String lastName, int socialSecurityNumber, double baseSalary, double totalSales, double commissionRate, double bonus) {
+    public BaseCommissionEmployee(String firstName, String lastName, int socialSecurityNumber, double baseSalary, double totalSales, double commissionRate) {
         super(firstName, lastName, socialSecurityNumber);
         this.baseSalary = baseSalary;
         this.totalSales = totalSales;
         this.commissionRate = commissionRate;
-        this.bonus = bonus;
     }
-
+    
     @Override
     public double income() {
-        return baseSalary + (totalSales * commissionRate) + bonus;
-    }
+        return baseSalary + (totalSales * commissionRate) + (BONUS_RATE * baseSalary);
+    }    
 }
-
