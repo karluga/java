18dfad59a177216028d8bd9f61bb854f8d340fa3
ADSkyler <anrijsdambis@gmail.com>
2024-11-19 00:14:38 +0200

    Overhaul Edit

--- cmd-projects/employee-salary-system-with-abstract-methods-and-polymorphysm/PartTimeEmployee.java
+++ cmd-projects/employee-salary-system-with-abstract-methods-and-polymorphysm/PartTimeEmployee.java
@@ -1,12 +1,21 @@
-
-
 class PartTimeEmployee extends Employee {
     private double hoursWorked;
     private double hourlyRate;
 
-    public PartTimeEmployee(String firstName, String lastName, int socialSecurityNumber, double hoursWorked, double hourlyRate) {
-        super(firstName, lastName, socialSecurityNumber);
+    public PartTimeEmployee(String firstName, String lastName, int ssn, double hoursWorked, double hourlyRate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = ssn;
         this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    // Setters
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
         this.hourlyRate = hourlyRate;
     }
 
@@ -15,3 +24,4 @@ class PartTimeEmployee extends Employee {
         return hoursWorked * hourlyRate;
     }
 }
+
