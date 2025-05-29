68e3ce60370521475319e3f59e4f9634fb0a198d
Kārlis Ivars Braķis <kaarlisbrakis@gmail.com>
2025-05-29 19:08:48 +0300

    specified the actual table

--- SpringBoot/HotelManagementWebVersion/src/main/java/com/may/informatic/entities/Payment.java
+++ SpringBoot/HotelManagementWebVersion/src/main/java/com/may/informatic/entities/Payment.java
@@ -5,8 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "payments")
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
